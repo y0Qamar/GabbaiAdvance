516c627fcafd9f2fa6844c6bf0ae954e107c7ab0
Qamar Yochanan <qamar.yochanan@gmail.com>
2022-11-21 11:06:45 -0600

    Ter: Protected refactor, Milah refactor, print method

--- src/GABBAI/Milah.java
+++ src/GABBAI/Milah.java
@@ -1,18 +1,117 @@
 package GABBAI;
 
-import java.util.HashMap;
+/*
+ Todo:
+  1) Shift from private to protected
+  2) add getters and setters
+  3) Abstracts
+  4) Prints
+  5) Test
+
+ */
 
 public class Milah extends Shoresh {
 
-    //variable for adults ONLY
-    boolean isSober;
+    //Adult congregant
+    protected String name; //name
+    protected int age;
+    protected String pNouns; //pronouns
+    protected String shevet; // tribe - who are your people--Can include interpreters, PCAs, caregivers, parents
+    protected String sup; //Support needs
+    protected String saf; //Safety needs
+    protected String accs; //Access needs
+    protected boolean isSober;
+    protected boolean aliyotReq;
 
-    //Constructor
-    public Milah(String name, int age, String pNouns, String shevet, String sup, String saf, String accs) {
-        super(name, age, pNouns, shevet, sup, saf, accs);
+    //contructor
+    public Milah(String name, int age, String pNouns, String shevet, String sup, String saf, String accs, boolean isSober, boolean aliyotReq) {
+        this.name = name;
+        this.age = age;
+        this.pNouns = pNouns;
+        this.shevet = shevet;
+        this.sup = sup;
+        this.saf = saf;
+        this.accs = accs;
+        this.isSober = isSober;
+        this.aliyotReq = aliyotReq;
     }
 
-//defines Hagafen
+//getters/settters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getpNouns() {
+        return pNouns;
+    }
+
+    public void setpNouns(String pNouns) {
+        this.pNouns = pNouns;
+    }
+
+    public String getShevet() {
+        return shevet;
+    }
+
+    public void setShevet(String shevet) {
+        this.shevet = shevet;
+    }
+
+    public String getSup() {
+        return sup;
+    }
+
+    public void setSup(String sup) {
+        this.sup = sup;
+    }
+
+    public String getSaf() {
+        return saf;
+    }
+
+    public void setSaf(String saf) {
+        this.saf = saf;
+    }
+
+    public String getAccs() {
+        return accs;
+    }
+
+    public void setAccs(String accs) {
+        this.accs = accs;
+    }
+
+    public boolean isSober() {
+        return isSober;
+    }
+
+    public void setSober(boolean sober) {
+        isSober = sober;
+    }
+
+    public boolean isAliyotReq() {
+        return aliyotReq;
+    }
+
+    public void setAliyotReq(boolean aliyotReq) {
+        this.aliyotReq = aliyotReq;
+    }
+
+
+
+    //Abstract A
     @Override
     public void bpHagefen() {
         if (isSober)
@@ -20,19 +119,27 @@ public class Milah extends Shoresh {
 
     }
 
-// Define aliyot requests
+    //Abstract B
     @Override
     public void aliyotReq() {
-        HashMap<String, String> aliyot = new HashMap(); //aliyot for Shabbat - reason, shabbat date
-
+        if(aliyotReq)
+        System.out.println("This person has requested an alia");//aliyot for Shabbat
     }
 
-
-    //print method for Milah
+    //Print method
     @Override
     public String toString() {
-        return super.toString();
-
+        return "Milah{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", pNouns='" + pNouns + '\'' +
+                ", shevet='" + shevet + '\'' +
+                ", sup='" + sup + '\'' +
+                ", saf='" + saf + '\'' +
+                ", accs='" + accs + '\'' +
+                ", isSober=" + isSober +
+                ", aliyotReq=" + aliyotReq +
+                '}';
     }
 }
 
--- src/GABBAI/Shoresh.java
+++ src/GABBAI/Shoresh.java
@@ -1,113 +1,16 @@
 package GABBAI;
-/* TODO: 11/18/22
-    1) Refactor to not include previous comments
-    2) Review all variables and methods
-    3) Pseudo-code refactor
-    4) Break
-    5) Refactor
+/*
 
 
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 abstract class Shoresh {
 
-    private String name; //name
-    private int age;
-    private String pNouns; //pronouns
-    private String shevet; // tribe - who are your people--Can include interpreters, PCAs, caregivers, parents
-    private String sup; //Support needs
-    private String saf; //Safety needs
-    private String accs; //Access needs
 
-   /* Methods I want aaliyot private HashMap<> aliyot; //Aliyahs request method make a method (this will be children under bne'i mitzavh age)
-   //  isSober; Make an empty method called hagafen - kids -pritns this person is underage, sober adults will print somn else
-
-    */
-
-    public Shoresh(String name, int age, String pNouns, String shevet, String sup, String saf, String accs) {
-        this.name = name;
-        this.age = age;
-        this.pNouns = pNouns;
-        this.shevet = shevet;
-        this.sup = sup;
-        this.saf = saf;
-        this.accs = accs;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getpNouns() {
-        return pNouns;
-    }
-
-    public void setpNouns(String pNouns) {
-        this.pNouns = pNouns;
-    }
-
-    public String getShevet() {
-        return shevet;
-    }
-
-    public void setShevet(String shevet) {
-        this.shevet = shevet;
-    }
-
-    public String getSup() {
-        return sup;
-    }
-
-    public void setSup(String sup) {
-        this.sup = sup;
-    }
-
-    public String getSaf() {
-        return saf;
-    }
-
-    public void setSaf(String saf) {
-        this.saf = saf;
-    }
-
-    public String getAccs() {
-        return accs;
-    }
-
-    public void setAccs(String accs) {
-        this.accs = accs;
-    }
-
+    //Mehtod for folks who are sober/underage - some are kids
     public abstract void bpHagefen(); //ot v milah
 
-    public abstract void aliyotReq(); //ot v milah
-
-    @Override
-    public String toString() {
-        return "Shoresh{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", pNouns='" + pNouns + '\'' +
-                ", shevet='" + shevet + '\'' +
-                ", sup='" + sup + '\'' +
-                ", saf='" + saf + '\'' +
-                ", accs='" + accs + '\'' +
-                '}';
-    }
+    //Method for when someone asks for Shabbat blessings - a child wants to do Kol HaNe'arim
+   public abstract void aliyotReq(); //ot v milah
 }
 
